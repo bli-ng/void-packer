ae5396270776685cd2bf310cac639c8570d6718d
Nicklaus McClendon <nicklaus@kulinacs.com>
2017-02-21 02:19:35 -0600

    Functional Base Vagrant Box

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,55 @@
-echo "root:rootpass" | chpasswd -c SHA512
+echo ">> Fix File Permissions"
 chown root:root /
-chmod 755 /
+chmod 0755 /
+
+echo ">> Add user: vagrant"
 useradd vagrant
 echo "vagrant:vagrant" | chpasswd -c SHA512
-echo "void-test" > /etc/hostname
-echo 'HOSTNAME="void-test"' > /etc/rc.conf
+
+echo ">> Configure vagrant ssh usage"
+mkdir -p /home/vagrant/.ssh
+chmod 0700 /home/vagrant/.ssh
+yes | xbps-install -Sy -R http://repo.voidlinux.eu/current curl
+curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
+echo ">> Give vagrant passwordless sudo access"
+echo "vagrant ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/vagrant
+chmod 0440 /etc/sudoers.d/vagrant
+
+echo ">> Set root password to vagrant"
+echo "root:vagrant" | chpasswd -c SHA512
+
+echo ">> Enable critical services"
+ln -s /etc/sv/dhcpcd /etc/runit/runsvdir/default/dhcpcd
+ln -s /etc/sv/sshd /etc/runit/runsvdir/default/sshd
+
+echo ">> Instal Virtualbox Guest Additions"
+xbps-install -Sy -R http://repo.voidlinux.eu/current virtualbox-ose-guest
+ln -s /etc/sv/vboxservce /etc/runit/runsvdir/default/vboxservice
+
+echo ">> Set hostname: voidlinux"
+echo "voidlinux" > /etc/hostname
+
+echo ">> Configure /etc/rc.conf"
+echo 'HOSTNAME="voidlinux"' > /etc/rc.conf
 echo 'HARDWARECLOCK="UTC"' >> /etc/rc.conf
 echo 'TIMEZONE="America/Chicago"' >> /etc/rc.conf
 echo 'KEYMAP="us"' >> /etc/rc.conf
 echo "/dev/sda1	 /boot	vfat	defaults	0	2" >> /etc/fstab
 echo "/dev/sda2	 /		ext4	defaults	0	1" >> /etc/fstab
+
+echo ">> Configure locales"
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /etc/default/libc-locales
-echo 'hostonly="yes"' >> /etc/dracut.conf
 xbps-reconfigure -f glibc-locales
+
+echo ">> Configure dracut"
+echo 'hostonly="yes"' >> /etc/dracut.conf
+
+echo ">> Install grub"
 grub-install /dev/sda
+
+echo ">> Configure Linux kernel"
 xbps-reconfigure -f linux4.9-4.9.11_1
